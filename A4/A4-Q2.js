7d85d2d727f1daae22be9cacbe7d48df0c2cb49d
Mohammed Nushoor Khan <65109457+Knight-Kaizen@users.noreply.github.com>
2021-01-27 23:17:57 +0530

    Add files via upload

---
+++ A4/A4-Q2.js
@@ -0,0 +1,10 @@
+function add()
+{
+    var a = document.getElementById("1st");
+    var b = document.getElementById("2nd");
+    var ans = parseInt(a.value) + parseInt(b.value);
+    document.getElementById("ans").value = ans;
+    
+}
+
+
